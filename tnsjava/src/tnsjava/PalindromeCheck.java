e3f4c773dd0b3b2e627a10025f8c3bbf59168509
srinivas <srinivas@localhost>
2025-09-22 20:42:36 +0530

    Palindrome

---
+++ tnsjava/src/tnsjava/PalindromeCheck.java
@@ -0,0 +1,20 @@
+package tnsjava;
+
+public class PalindromeCheck {
+	 public static void main(String[] args) {
+	 String str = "madam";
+	 System.out.println(isPalindrome(str));
+	 }
+
+	 public static boolean isPalindrome(String str) {
+	 int left = 0, right = str.length() - 1;
+	 while (left < right) {
+	 if (str.charAt(left) != str.charAt(right)) {
+	 return false;
+	 }
+	 left++;
+	 right--;
+	 }
+	 return true;
+	 }
+	}
