76294bba79af7f0706350e9c5243ab3f31033862
srinivas <srinivas@localhost>
2025-09-28 18:41:22 +0530

    Array

---
+++ tnsjava/src/arrays/StaticArray.java
@@ -0,0 +1,13 @@
+package arrays;
+
+public class StaticArray {
+
+	public static void main(String[] args) {
+			int[][] nums = new int[3][];  //represents of 3 rows but not fixed columns
+			
+			nums[0] = new int[3];
+			nums[1] = new int[4];
+			nums[2] = new int[2];
+
+	}
+}
