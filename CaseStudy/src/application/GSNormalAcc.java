01497a4341be950613804490cc220914a9834439
srinivas <srinivas@localhost>
2025-10-10 18:50:58 +0530

    casestudy

---
+++ CaseStudy/src/application/GSNormalAcc.java
@@ -0,0 +1,25 @@
+package application;
+
+import framework.NormalAcc;
+
+public class GSNormalAcc extends NormalAcc{
+
+	public GSNormalAcc(int accNo, String accNm, float charges, float deliveryCharge) {
+		super(accNo, accNm, charges, deliveryCharge);
+		
+	}
+
+	public void bookProduct(float charges)
+	{
+	System.out.println("Hy user your charges are : "+charges +"and the deliverycharges are : "+deliveryCharge );	
+	
+	}
+
+	@Override
+	public String toString() {
+		return "GSNormalAcc [deliveryCharge=" + deliveryCharge + ", accNo=" + accNo + ", accNm=" + accNm + ", charges="
+				+ charges + "]";
+	}
+	
+	
+}
