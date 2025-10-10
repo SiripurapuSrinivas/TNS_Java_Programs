96e2ca7c821a207824bf1f1d29eed39269354ec1
srinivas <srinivas@localhost>
2025-10-10 11:33:02 +0530

    Lambda

---
+++ tnsjava/src/Lambda/LambdaAnnotationsExp.java
@@ -0,0 +1,21 @@
+package Lambda;
+
+//Functional interface annotated with @FunctionalInterface
+@FunctionalInterface
+interface Greeting {
+void sayHello(String name);
+}
+
+public class LambdaAnnotationsExp {
+
+	public static void main(String[] args) {
+		// Lambda expression implementing the functional interface
+	     Greeting greet = name -> System.out.println("Hello, " + name + "! Welcome.");
+
+	     // Call the lambda
+	     greet.sayHello("Srinu");
+	     greet.sayHello("Vasu");
+
+	}
+
+}
