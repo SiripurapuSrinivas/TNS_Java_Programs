a3aa23c37fa4280ba5802f155c48ced668d6ad91
srinivas <srinivas@localhost>
2025-10-10 14:15:21 +0530

    checked

---
+++ tnsjava/src/exceptions/CheckedExceptions.java
@@ -0,0 +1,14 @@
+package exceptions;
+
+import java.io.*;
+
+public class CheckedExceptions {
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		File f = new File("Hello.txt");
+		FileReader fr = new FileReader(f);
+		
+
+	}
+}
